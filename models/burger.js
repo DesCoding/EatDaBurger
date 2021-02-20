812c3abe81bb45e5db0b42bfd38b15aeded7c09c
Des <d.stout31@yahoo.com>
2021-02-19 21:12:40 -0600

    required orm to create functions to interact with db and export to controller

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+//Import ORM to create functions that will interact with the burgers database
+const orm = require("../config/orm.js");
+
+const burger = {
+  all(cb) {
+    orm.all("burgers", (res) => cb(res));
+  },
+  create(cols, vals, cb) {
+    orm.create("burgers", cols, vals, (res) => cb(res));
+  },
+  update(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.delete("burgers", condition, (res) => cb(res));
+  },
+};
+
+//Export the database functions for the controler burgers_controller.js
+module.exports = burger;
