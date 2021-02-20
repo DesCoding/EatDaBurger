118f10aae76aa1f72b0c77d815caab41929c3481
Des <d.stout31@yahoo.com>
2021-02-19 21:02:58 -0600

    adding in more seeds/burger options

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
-INSERT INTO cats (name) VALUES ('Charlie');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chickpea Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
 
