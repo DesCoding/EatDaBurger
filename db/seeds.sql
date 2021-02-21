ebae1c38431932ae5394a10a06f3e4317350d284
Des <d.stout31@yahoo.com>
2021-02-21 15:05:34 -0600

    updated css formatting, fixed alignment of ol items, formatted columns in index handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,1 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chickpea Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
 
